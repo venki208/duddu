67b2a1a26acafb983d6b1cf9e93e884a13ab1d59
venkatesh.duddu <venkatesh.duddu@mobisir.net>
2017-02-22 18:19:48 +0530

    added angular js files

--- duddu/duddu/settings.py
+++ duddu/duddu/settings.py
@@ -150,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
--- duddu/user_signup/urls.py
+++ duddu/user_signup/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	url(r'^$', views.index, name='signup'),
+	url(r'^home/$', TemplateView.as_view(template_name="home.html")),
 ]
