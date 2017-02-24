af32382c90e5ef915592e31c7bb9900481f20468
venkatesh.duddu <venkatesh.duddu@mobisir.net>
2017-02-24 16:14:45 +0530

    added requirements

--- duddu/user_signup/urls.py
+++ duddu/user_signup/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
 from django.views.generic import TemplateView
-
+from .views import SignupForm
 urlpatterns = [
 	url(r'^$', views.index, name='signup'),
 	url(r'^home/$', TemplateView.as_view(template_name="home.html")),
+	url(r'^user_signup/', SignupForm.as_view()),
 ]
--- duddu/user_signup/views.py
+++ duddu/user_signup/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponse, Http404
 from django.template import loader
 from Student.models import Question
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 # Create your views here.
 @login_required()
 def index(request):
 	return render(request, 'index.html', locals())
+
+class SignupForm(TemplateView):
+	template_name = "user_signup/signup.html"
